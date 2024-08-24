b4657e6956b2efd819f5d327ae8c0676334e7c88
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-08-24 15:40:00 -0400

    13 Navbar update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from './components/Navbar';
 import './globals.css';
 
 export default function RootLayout({
@@ -8,8 +9,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <nav>Hello There</nav>
-        {children}
+        <Navbar />
+        <main className="max-w-3xl mx-auto py-20">{children}</main>
       </body>
     </html>
   );
