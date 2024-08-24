b06fb31d69c846eafc8900626f039ca42014821e
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-08-24 15:49:58 -0400

    15 Metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
 import './globals.css';
 import Navbar from './components/Navbar';
-import { Inter, Inconsolata, Roboto } from 'next/font/google';
+import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
-const inconsolata = Inconsolata({ subsets: ['latin'] });
-const roboto = Roboto({ subsets: ['latin'], weight: ['400'] });
+
+export const metadata: Metadata = {
+  title: 'Next.js Project',
+  description: 'A Next.js project with TypeScript and TailwindCSS.',
+  keywords: 'Next.js, Typescript, TailwindCSS',
+};
 
 export default function RootLayout({
   children,
@@ -13,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={roboto.className}>
+      <body className={inter.className}>
         <Navbar />
         <main className="max-w-3xl mx-auto py-20">{children}</main>
       </body>
