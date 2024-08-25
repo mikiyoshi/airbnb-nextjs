a62766ed0b5436b93b8ca0debe4f2d7154c5504d
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-08-24 21:51:08 -0400

    25 Remote Images update

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,5 +1,6 @@
 import mapsImg from '@/images/maps.jpg';
 import Image from 'next/image';
+const url = 'https://www.course-api.com/images/tours/tour-1.jpeg';
 
 const page = ({ params }: { params: { id: string } }) => {
   console.log(params);
@@ -19,8 +20,18 @@ const page = ({ params }: { params: { id: string } }) => {
             className="w-48 h-48 object-cover rounded"
           />
           <h2>local image</h2>
-          {/* remote image */}
-          <div></div>
+        </div>
+        {/* remote image */}
+        <div>
+          <Image
+            src={url}
+            alt="tour"
+            width={192}
+            height={192}
+            priority
+            className="w-48 h-48 object-cover rounded"
+          />
+          <h2>remote image</h2>
         </div>
       </section>
     </div>
