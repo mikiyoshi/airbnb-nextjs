2dd2eb7b4c2177ee92c8691a1a1951014ac0a6ae
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-08-24 21:46:27 -0400

    24 Local Image update

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,9 +1,28 @@
+import mapsImg from '@/images/maps.jpg';
+import Image from 'next/image';
+
 const page = ({ params }: { params: { id: string } }) => {
   console.log(params);
 
   return (
     <div>
       <h1 className="text-4xl">ID : {params.id}</h1>
+      <section className="flex gap-x-4 mt-4">
+        {/* local image */}
+        <div>
+          <Image
+            src={mapsImg}
+            alt="maps"
+            priority
+            width={192}
+            height={192}
+            className="w-48 h-48 object-cover rounded"
+          />
+          <h2>local image</h2>
+          {/* remote image */}
+          <div></div>
+        </div>
+      </section>
     </div>
   );
 };
