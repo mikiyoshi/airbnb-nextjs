cbd8bbf43ce81658197689b299fc147b547678c9
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-08-25 11:23:43 -0400

    30 Actions File update

---
+++ utils/actions.ts
@@ -0,0 +1,11 @@
+'use server';
+
+export const createUser = async (formData: FormData) => {
+  'use server';
+
+  const firstName = formData.get('firstName') as string;
+  const lastName = formData.get('lastName') as string;
+  console.log({ firstName, lastName });
+  const rawData = Object.fromEntries(formData);
+  console.log(rawData);
+};
