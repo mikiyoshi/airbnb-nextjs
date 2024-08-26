5f367f690d6d1956f752902367588885d4946263
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-08-26 12:03:33 -0400

    41 Route Handlers - Param update

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,16 +1,10 @@
-// the following HTTP methods are supported: GET, POST, PUT, PATCH, DELETE, HEAD, and OPTIONS. If an unsupported method is called, Next.js will return a 405 Method Not Allowed response.
-
-import { NextRequest, NextResponse } from 'next/server';
 import { fetchUsers, saveUser } from '@/utils/actions';
+import { NextRequest, NextResponse } from 'next/server';
 
-export const GET = async () => {
+export const GET = async (req: NextRequest) => {
+  console.log(req.url);
+  console.log(req.nextUrl.searchParams.get('id'));
+
   const users = await fetchUsers();
-  return Response.json({ users });
-};
-
-export const POST = async (request: Request) => {
-  const user = await request.json();
-  const newUser = { ...user, id: Date.now().toString() };
-  await saveUser(newUser);
-  return Response.json({ msg: 'user created' });
+  return NextResponse.redirect(new URL('/', req.url));
 };
