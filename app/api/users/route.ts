c1745276b337d620594549283e87ff5a93bedb08
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-08-26 13:43:16 -0400

    42 Route Handlers - POST update

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -6,5 +6,15 @@ export const GET = async (req: NextRequest) => {
   console.log(req.nextUrl.searchParams.get('id'));
 
   const users = await fetchUsers();
-  return NextResponse.redirect(new URL('/', req.url));
+  // return NextResponse.redirect(new URL('/', req.url));
+  return Response.json({ users });
+};
+
+export const POST = async (req: Request) => {
+  // const result = await req.json();
+  // console.log(result);
+  const user = await req.json();
+  const newUser = { ...user, id: Date.now().toString() };
+  await saveUser(newUser);
+  return Response.json({ msg: 'user created' });
 };
