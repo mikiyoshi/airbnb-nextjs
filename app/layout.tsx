450d814a1b2fa1640ca9840d9ef4657295ec854a
mikiyoshi <mikiyoshi.kokura@gmail.com>
2024-08-27 10:17:46 -0400

    44 Local Build update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import Navbar from '../components/Navbar';
-import { Metadata } from 'next';
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -13,9 +13,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={inter.className}>
--- components/UsersList.tsx
+++ components/UsersList.tsx
@@ -6,16 +6,18 @@ async function UsersList() {
   return (
     <div className="mt-4">
       {users.length ? (
-        <div>
-          {users.map((user) => (
-            <h4
-              key={user.id}
-              className="capitalize text-lg flex justify-between items-center mb-2"
-            >
-              {user.firstName} {user.lastName}
-              <DeleteButton id={user.id} />
-            </h4>
-          ))}
+        <div className="max-w-lg">
+          {users.map((user) => {
+            return (
+              <h4
+                key={user.id}
+                className="capitalize text-lg flex justify-between items-center mb-2"
+              >
+                {user.firstName} {user.lastName}
+                <DeleteButton id={user.id} />
+              </h4>
+            );
+          })}
         </div>
       ) : (
         <p>No users found...</p>
